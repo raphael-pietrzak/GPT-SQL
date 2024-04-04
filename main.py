0350291e8e190880bdfdc733de944f9e433cfee2
Robin Violet <robin.violet.rv@gmail.com>
2024-04-04 11:19:21 +0200

    menu-feat(main):Text extraction & verification testing

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-
+from text import Text
 
 class Main:
     def __init__(self):
-        pass
+        self.text = Text()
 
     def run(self):
-        pass
+
+        # Text extraction & verification testing
+        path = ""
+        self.text.initialize(path)
+        print(self.text.content)
 
 
 if __name__ == '__main__':
