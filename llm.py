be07c7667e0345eccbd21389059350c77074e526
PIETRZAK Raphael <raphael.pietrzak@etu.univ-amu.fr>
2024-04-05 02:47:38 +0200

    feat(llm) : prompt

--- llm.py
+++ llm.py
@@ -4,16 +4,34 @@ from gpt4all import GPT4All
 class Llm:
     def __init__(self):
         # model
+        # self.model = GPT4All('orca-mini-3b-gguf2-q4_0.gguf')
         self.model = GPT4All('mistral-7b-instruct-v0.1.Q4_0.gguf')
         # story
         self.path = 'stories/story2.txt'
         self.story = open(self.path, 'r').read()
 
-        self.columns = ['verbs']
+        self.columns = ['verbes']
 
 
 
     def generate(self):
-        prompt = f"Réponds moi uniquement par [oui] ou [non]. Est ce que cette histoire contient des {self.columns[0]} ? \n HISTOIRE : {self.story}\n"
-        output = self.model.generate(prompt)
+        # prompt = f"Formatte moi sous format json l'histoire suivante : \n HISTOIRE : {self.story}\n avec comme paramètre de recherche les {self.columns[0]}\n"
+        # prompt = f"Réponds moi uniquement par [oui] ou [non]. Est ce que cette histoire : '{self.story}' contient le mot 'prairie' ?"
+        prompt = """Tâche : Créez une sortie json à partir d'une histoire en entrée et renvoie les verbes présents sous forme de liste JSON.
+
+        Entrée : Il était une fois un chevalier qui courait dans la prairie.
+
+        Sortie : Une liste JSON des verbes extraits de l'histoire.
+
+        Instructions : Traitez l'entrée pour extraire les verbes et retournez-les en tant que liste JSON sans doublons, ordonnée alphabétiquement.
+
+        Exemple :
+
+        Entrée : 'Jean marchait lentement vers la rivière.'
+
+        Sortie : {'verbes': ['marchait']}"""
+
+
+        output = self.model.generate(prompt, max_tokens=100)
         return output
+
